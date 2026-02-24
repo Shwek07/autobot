048ebb195429a23f250e1b0321f33973de6359a4
Shwek07 <shakeelramdhiansing@gmail.com>
2026-02-24 11:17:24 -0300

    proxy to middelware again

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt";
 
 export const runtime = "nodejs";
 
-export async function proxy(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
