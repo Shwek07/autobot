4c69ec3bad787cfc041feb6d523daf217dac4ac8
Shwek07 <shakeelramdhiansing@gmail.com>
2026-02-24 12:07:33 -0300

    updated layout in users

---
+++ app/(users)/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+// app/admin/layout.tsx
+
+
+import Sidebar from "@/components/dashboard/Sidebar";
+import styles from "./usersGlobal.module.css";
+
+export default function UsersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={styles.layout}>
+      <Sidebar />
+
+      <div className={styles.main}>
+
+        <div className={styles.content}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
