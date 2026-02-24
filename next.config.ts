d4868463c42d8e84c5c2840262ee40e756faff28
Shwek07 <shakeelramdhiansing@gmail.com>
2026-02-24 10:23:06 -0300

    error handeling for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.js
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
